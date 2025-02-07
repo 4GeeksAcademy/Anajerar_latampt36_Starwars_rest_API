bc4c96df75fe0356b99274ea13da2d024da08314
Anajerar <aurelio.najerar@gmail.com>
2025-02-07 04:44:15 +0000

    First backup

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,182 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['POST'])
+def addPeople():
+    data = request.json
+    print('Json Data name:', type(data),data)
+    name = data.get('name')
+    birth_year = data.get('birth_year')
+    gender = data.get('gender')
+    height = data.get('height')
+    hair_color = data.get('hair_color')
+    homeworld = data.get('homeworld')
+
+    people_exist = db.session.execute(db.select(People).filter_by(name=name)).one_or_none()
+    if people_exist==None:
+        new_people = People(
+            name = name,
+            birth_year = birth_year,
+            gender = gender,
+            height = height,
+            hair_color = hair_color,
+            homeworld = homeworld
+            )
+        try:
+            db.session.add(new_people)
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            print('Error:',error)
+            return jsonify({"message": "Error saving People to database"}), 500
+
+        return jsonify({
+            "user": new_people.serialize(),
+            "message": "ok"
+        }), 200
+    else:
+        return jsonify({"msg":"People already exist"}),400
+
+@app.route('/user', methods=['POST'])
+def addUser():
+    data = request.json
+    email = data.get('email')
+    user_name = data.get('user_name')
+    full_name = data.get('full_name')
+    password = data.get('password')
+    
+    user_exist = db.session.execute(db.select(User).filter_by(email=email)).one_or_none()
+    if user_exist==None:
+        new_user = User(
+            email = email,
+            user_name = user_name,
+            full_name = full_name,
+            password = password
+            )
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            print('Error:',error)
+            return jsonify({"message": "Error saving user to database"}), 500
+
+        return jsonify({
+            "user": new_user.serialize(),
+            "message": "ok"
+        }), 200
+    else:
+        return jsonify({"msg":"Planet already exist"}),400
+
+@app.route('/users',methods=['GET'])
+def listUsers():
+    users_list=[]
+    users = db.session.execute(db.select(User)).all()
+    for user in users:
+        users_list.append(user[0].serialize())
+    return jsonify({'msg':'ok','users':users_list})
+
+@app.route('/users/<int:id>',methods=['GET'])
+def singleUser(id):
+    user = db.session.execute(db.select(User).filter_by(id=id)).one_or_none();
+    if user == None:
+        return jsonify({"msg":"user not found"}),404
+    else:
+        print("This is the user",user[0].serialize())
+        return jsonify({"msg":"ok","user":user[0].serialize()})
+
+@app.route('/planet', methods=['POST'])
+def addPlanet():
+    data = request.json
+    print('Json Data name:', type(data),data)
+    planet_name = data.get('planet_name')
+    population = data.get('population')
+    climate = data.get('climate')
+    diameter = data.get('diameter')
+    gravity = data.get('gravity')
+    picture_url = data.get('picture_url')
+
+    planet_exist = db.session.execute(db.select(Planets).filter_by(planet_name=planet_name)).one_or_none()
+    if planet_exist==None:
+        new_planet = Planets(
+            planet_name = planet_name,
+            population = population,
+            climate = climate,
+            diameter = diameter,
+            gravity = gravity,
+            picture_url = picture_url
+            )
+        try:
+            db.session.add(new_planet)
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            print('Error:',error)
+            return jsonify({"message": "Error saving Planet to database"}), 500
+
+        return jsonify({
+            "user": new_planet.serialize(),
+            "message": "ok"
+        }), 200
+    else:
+        return jsonify({"msg":"Planet already exist"}),400
+
+@app.route('/planets', methods=['GET'])
+def listPlanets():
+    planets_list=[]
+    planets = db.session.execute(db.select(Planets)).all()
+    for planet in planets:
+        planets_list.append(planet[0].serialize())
+    return jsonify({'msg':'ok','users':planets_list})
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def singlePlanet(id):
+    planet = db.session.execute(db.select(Planets).filter_by(id=id)).one_or_none();
+    if planet == None:
+        return jsonify({"msg":"planet not found"}),404
+    else:
+        return jsonify({"msg":"ok","planet":planet[0].serialize()})
+
+
+@app.route('/people', methods=['GET'])
+def people():
+    people_list=[]
+    people = db.session.execute(db.select(People)).all()
+    for single_person in people:
+        people_list.append(single_person[0].serialize())
+    return jsonify({'msg':'ok','users':people_list})
+    
+
+@app.route('/people/<int:people_id>',methods=['GET'])
+def single_person(people_id):
+    person = db.session.execute(db.select(People).filter_by(id=people_id)).one_or_none();
+    if person == None:
+        return jsonify({"msg":"person not found"}),404
+    else:
+        return jsonify({"msg":"ok","planet":person[0].serialize()})
+
+@app.route('/users/favorites',methods=['GET'])
+def user_favorites():
+    return {"msg":"under construction"},200
+
+@app.route('/user/favorite/<int:planet_id>',methods=['POST'])
+def user_fav_planet(planet_id):
+    
+    return{"msg":"under construction"},200
+
+@app.route('/user/favorite/<int:people_id>',methods=['POST'])
+def user_fav_people(people_id):
+    return {"msg":"under construction"},200
+
+@app.route('/user/favorite/<int:planet_id>',methods=['DELETE'])
+def user_del_planet(planet_id):
+    return{"msg":"under construction"},200
+
+@app.route('/user/favorite/<int:people_id>',methods=['DELETE'])
+def user_del_people(people_id):
+    return {"msg":"under construction"},200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,99 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    user_name = db.Column(db.String(50), nullable=False)
+    full_name = db.Column(db.String(100))
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    fav_planet = db.relationship('FavoritePlanets',back_populates='user')
+
+    def __init__(self,email,user_name,full_name,password):
+        self.email = email
+        self.user_name = user_name
+        self.full_name = full_name
+        self.password = password
+        self.is_active = True
+
+    #def __repr__(self):
+    #    return self.full_name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "user_name": self.user_name,
+            "full_name": self.full_name
             # do not serialize the password, its a security breach
         }
+    
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(50), unique=True)
+    population = db.Column(db.String(40),nullable=True)
+    climate = db.Column(db.String(25),nullable=True)
+    diameter = db.Column(db.Integer,nullable=True)
+    gravity = db.Column(db.String(15),nullable=True)
+    picture_url = db.Column(db.String(300),nullable=True)
+
+    planet_fav = db.relationship('FavoritePlanets', back_populates='planet')
+
+    def __repr__(self):
+        return '<Planet %r>' % self.planet_name
+    
+    def serialize(self):
+        return {
+            "planet": self.planet_name,
+            "population": self.population,
+            "picture_url":self.picture_url,
+            "climate":self.climate,
+            "diameter":self.diameter,
+            "gravity":self.diameter,
+            "id":self.id
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    birth_year = db.Column(db.String(15), nullable=False)
+    gender = db.Column(db.String(10),nullable=True)
+    height = db.Column(db.String(10),nullable=True)
+    hair_color = db.Column(db.String(10),nullable=True)
+    homeworld = db.Column(db.String(25), nullable=True)
+    picture_url = db.Column(db.String(300), nullable=True)
+    
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "homeworld":self.homeworld,
+            "picture_url": self.picture_url
+        }
+
+class FavoritePlanets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_fav_id = db.Column(db.Integer, ForeignKey(Planets.id))
+    user_fav_id = db.Column(db.Integer,ForeignKey(User.id))
+    user = db.relationship(User, back_populates='fav_planet')
+    planet = db.relationship(Planets, back_populates='planet_fav')
+
+class FavoritePeople(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    people_fav_id = db.Column(db.Integer, ForeignKey(People.id))
+    user_fav_id = db.Column(db.Integer,ForeignKey(User.id))
+    user = db.relationship(User)
+    people = db.relationship(People)
+
+    def to_dict(self):
+        return {}
